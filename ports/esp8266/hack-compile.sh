0c09b4ec7e0ffff2b14193880995bfe6c77dcfcf
JJ on UbuntuWSL <jj@gioorgi.com>
2019-05-19 19:36:43 +0200

    Uasyncio with 40Kb experimental kernel

--- ports/esp8266/hack-compile.sh
+++ ports/esp8266/hack-compile.sh
@@ -1,5 +1,6 @@
 
 export PATH=/home/jj/code/esp8266/esp8266-toolchain-wsl/xtensa-lx106-elf/bin:$PATH
 
-make -j 2
+make -j 2 V=1
 cp build/firmware-combined.bin /home/jj/code/esp8266-iot-monitor/firmware/esp8266-experimental-v1.10.bin
+
